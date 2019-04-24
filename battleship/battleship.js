cc34375bf1eb41996eaf5983dd378a93face156c
Denis Novikov <voothi@gmail.com>
2019-04-24 15:32:50 +0300

    &&

--- battleship/battleship.js
+++ battleship/battleship.js
@@ -1,12 +1,6 @@
 var isSock = prompt("isSock");
 var isSale = prompt("isSale");
 
-if (isSock == true) {
-    if (isSale == true) {
-        alert("Купить!");
-    } else {
-        alert("isSale is " + isSale)
-    }
-} else {
-    alert("isSock is " + isSock)
+if (isSock == true && isSale == true) {
+    alert("Купить!");
 }

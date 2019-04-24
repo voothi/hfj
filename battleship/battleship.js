b947f392b7772260cb9fb15ce61318d3fa00665d
Denis Novikov <voothi@gmail.com>
2019-04-24 15:05:27 +0300

    boolean test

--- battleship/battleship.js
+++ battleship/battleship.js
@@ -1,35 +1,12 @@
-var location1 = 3;
-var location2 = 4;
-var location3 = 5;
+var isSock = prompt("isSock");
+var isSale = prompt("isSale");
 
-var guess;
-var hits = 0;
-var guesses = 0;
-
-var isSunk = false;
-
-while (isSunk == false) {
-    guess = prompt("Готовноть, прицел, огонь! (введите число 0-6:)");
-
-    if (guess < 0 || guess > 6) {
-        alert("Введите привильное значение - число 0-6:");
+if (isSock == true) {
+    if (isSale == true) {
+        alert("Купить!");
     } else {
-        guesses++;
-        alert("Число догадок: " + guesses);
-
-        if (guess == location1 || guess == location2 || guess == location3) {
-            alert("Точное попадание!");
-            
-            hits++;
-
-            if (hits == 3) {
-                isSunk = true;
-                alert("Корабль потоплен!");
-            }
-        } else {
-            alert("Мазила!");
-        }
+        alert("isSale is " + isSale)
     }
-    var stats = "Вы выстрелили " + guesses + " раз для потопления корабля. " + "Ваша точность составила " + (3 / guesses)
-    alert(stats);
+} else {
+    alert("isSock is " + isSock)
 }

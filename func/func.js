173223e5068b8a9e9197441f6d9bc168eeca6e52
Denis Novikov <voothi@gmail.com>
2019-04-29 17:48:16 +0300

    doIt

--- func/func.js
+++ func/func.js
@@ -1,19 +1,6 @@
-function bark(name, weight) {
-    if (weight > 20) {
-        console.log(name + " says WOOF WOOF");
-    } else {
-        console.log(name + " says woof woof");
-    }
+function doIt(param) {
+    param = 2;
 }
-
-bark("rover", 23);
-bark("spot", 13);
-bark("spike", 53);
-bark("lady", 17);
-
-bark("jino", 20);
-bark("scottie", -1);
-bark("dino", 0, 0);
-bark("fido", 20);
-bark("lady", 10);
-bark("bruno", 21);
+var test = 1;
+doIt(test);
+console.log(test);

3813c709823de6afccdc84865be341426804234e
Denis Novikov <voothi@gmail.com>
2019-04-29 15:51:12 +0300

    ex pen

--- func/func.js
+++ func/func.js
@@ -10,3 +10,10 @@ bark("rover", 23);
 bark("spot", 13);
 bark("spike", 53);
 bark("lady", 17);
+
+bark("jino", 20);
+bark("scottie", -1);
+bark("dino", 0, 0);
+bark("fido", 20);
+bark("lady", 10);
+bark("bruno", 21);

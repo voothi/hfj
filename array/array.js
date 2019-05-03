55dca1db784b14e3d41328219d164bf8b850cc1d
Denis Novikov <voothi@gmail.com>
2019-05-03 23:42:07 +0300

    highest

--- array/array.js
+++ array/array.js
@@ -20,7 +20,10 @@ for (var i = 0; i < scores.length; i++) {
 console.log("Bubbles tests: " + scores.length);
 console.log("Highest bubble score: " + highScore);
 
-var genres  = [];
-genres.push("Metal");
-genres.push("Classic");
-var size = genres.length
+var bestSolutions = [];
+for (var i = 0; i < scores.length; i++) {
+    if (scores[i] == highScore) {
+        bestSolutions.push(i);
+    }
+}
+console.log("Solutions with the highest score: " + bestSolutions);

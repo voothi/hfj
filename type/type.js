f5c82c437e8821c731cdda6439203cc95a685e9e
Denis Novikov <voothi@gmail.com>
2019-05-05 14:28:12 +0300

    instanceof

--- type/type.js
+++ type/type.js
@@ -1,52 +1,8 @@
-function lieDetectorTest() {
-    var lies = 0;
-    var stolenDiamond = {};
-    if (stolenDiamond) {
-        console.log("You stole the diamond");
-        lies++;
-    }
-    // пустой объект, true +1
-    
-    var car = {
-        keysInPocket: null
-    };
-    
-    if (car.keysInPocket) {
-        console.log("Uh oh, guess you stole the car!");
-        lies++;
-    }
-    // null, false
-    
-    if (car.emptyGasTank) {
-        console.log("You drove the car after you stole it!");
-        lies++;
-    }
-    // несуществующее свойство объекта - undef, false
-
-    var foundYouAtTheCrimeScene = [];
-    if (foundYouAtTheCrimeScene) {
-        console.log("A sure sign of guilt");
-        lies++;
-    }
-    // пустой массив, true
-
-    if (foundYouAtTheCrimeScene[0]) {
-        console.log("Caught with a stolen item!");
-        lies++;
-    }
-    // первый эл массива, false
-
-    var yourName = " ";
-    if (yourName) {
-        console.log("Guess you lied about your name");
-        lies++;
-    }
-    // не пустая строка, true
-
-    return lies;
+function Duck(sound) {
+    this.sound = sound;
+    this.quack = function () { console.log(this.sound); }
 }
-var numberOfLies = lieDetectorTest();
-console.log("You told " + numberOfLies + " lies!");
-if (numberOfLies >= 3) {
-    console.log("Guilty as charged");
-}
+var toy = new Duck("quack quack");
+toy.quack();
+console.log(typeof toy);
+console.log(toy instanceof Duck);

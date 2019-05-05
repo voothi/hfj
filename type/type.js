d65fe6c6c42be35d627056c0b82202b4b9800b2d
Denis Novikov <voothi@gmail.com>
2019-05-05 09:11:06 +0300

    null

--- type/type.js
+++ type/type.js
@@ -7,6 +7,7 @@ var test6;
 var test7 = { "abcdef": 123 };
 var test8 = ["abcdef", 123];
 function test9() { return "abcdef" };
+var test10 = null;
 
 console.log(typeof test1);
 console.log(typeof test2);
@@ -17,3 +18,4 @@ console.log(typeof test6);
 console.log(typeof test7);
 console.log(typeof test8);
 console.log(typeof test9);
+console.log(" " + typeof test10);

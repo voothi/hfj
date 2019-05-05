e4f3b61e67de77dc2beeb7a98b3903af2e0569e3
Denis Novikov <voothi@gmail.com>
2019-05-05 13:11:51 +0300

    lie 319

--- type/type.js
+++ type/type.js
@@ -1,5 +1,52 @@
-if (99 == "99a") {
-    console.log("Число равено строке!");
-} else {
-    console.log("Число не равно строке никогда!")
+function lieDetectorTest() {
+    var lies = 0;
+    var stolenDiamond = {};
+    if (stolenDiamond) {
+        console.log("You stole the diamond");
+        lies++;
+    }
+    // пустой объект, true +1
+    
+    var car = {
+        keysInPocket: null
+    };
+    
+    if (car.keysInPocket) {
+        console.log("Uh oh, guess you stole the car!");
+        lies++;
+    }
+    // null, false
+    
+    if (car.emptyGasTank) {
+        console.log("You drove the car after you stole it!");
+        lies++;
+    }
+    // несуществующее свойство объекта - undef, false
+
+    var foundYouAtTheCrimeScene = [];
+    if (foundYouAtTheCrimeScene) {
+        console.log("A sure sign of guilt");
+        lies++;
+    }
+    // пустой массив, true
+
+    if (foundYouAtTheCrimeScene[0]) {
+        console.log("Caught with a stolen item!");
+        lies++;
+    }
+    // первый эл массива, false
+
+    var yourName = " ";
+    if (yourName) {
+        console.log("Guess you lied about your name");
+        lies++;
+    }
+    // не пустая строка, true
+
+    return lies;
+}
+var numberOfLies = lieDetectorTest();
+console.log("You told " + numberOfLies + " lies!");
+if (numberOfLies >= 3) {
+    console.log("Guilty as charged");
 }
